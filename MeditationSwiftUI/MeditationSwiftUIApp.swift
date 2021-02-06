af58d8456e9bf4012bc251b653d5905b8df7ff38
Сергей Климович <sergei.klimovich@gmail.com>
2021-02-06 22:10:43 +0300

    Initial Commit

---
+++ MeditationSwiftUI/MeditationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationSwiftUIApp.swift
+//  MeditationSwiftUI
+//
+//  Created by Администратор on 06.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeditationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
